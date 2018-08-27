1255031db2dff10d9dba0e43bdd8b532a5a6abc8
Unknown <greisonsantos03@gmail.com>
2018-08-27 16:38:52 -0300

    tratando requisição pela url

--- noticias.js
+++ noticias.js
@@ -5,8 +5,24 @@ var http= require('http'); //incorpora biblioteca http no script
 //   res.end("<html><body>POrtal de noticias </body> </html>");    //resposta da requisição geralmente arquivos html
 //}).listen(3000); 
 
- var server = http.createServer(function(req, res){   //sobe um servidor
-   res.end("<html><body>Portal de notícias </body> </html>");    //resposta da requisição geralmente arquivos html
-}); 
 
- server.listen(3000);
+//tratanto requisições da url
+ var server = http.createServer(function(req, res){   //sobe um servidor
+
+var categoria= req.url;
+
+if (categoria=='/tecnologia'){
+
+	    res.end("<html><body>Noticias de Tecnologias</body> </html>");    //resposta da requisição geralmente arquivos html
+
+}else if(categoria=='/moda'){
+	   res.end("<html><body>Noticias de Moda</body> </html>");    //resposta da requisição geralmente arquivos html
+
+}else if(categoria=='/beleza'){
+	      res.end("<html><body>Noticias de Beleza </body> </html>");    //resposta da requisição geralmente arquivos html
+}else{
+	   res.end("<html><body>Portal de notícias </body> </html>");    //resposta da requisição geralmente arquivos html
+
+}
+ 
+}).listen(3000);
