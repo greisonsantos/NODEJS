3ac8b480272861acd5f259a4992082a4479af2de
Unknown <greisonsantos03@gmail.com>
2018-08-31 20:55:03 -0300

    consign para autoload de rotas

--- config/server.js
+++ config/server.js
@@ -1,6 +1,13 @@
-var express = require('express');
+
+var express = require('express'); //iniciando o mudulo do express
 var app = express();
-app.set('view engine', 'ejs');
-app.set('views', './app/views');
+
+var consign = require('consign'); //faço consign a olhar para dentro do diretorio routes e automaticamnete e realizado os requires
+
+app.set('view engine', 'ejs'); //indica que estou ultilizando ejs para rederizar as views
+app.set('views', './app/views'); //indica o caminho onde estar minha views
+
+consign().include('app/routes').into(app); //instancia cinsign apontando para routes e passado como parametro para execusão do express
+
 
 module.exports = app;
